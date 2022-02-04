f370ad1c1b5a6cf5c8906bc0f0b7ef2a35a172ba
Akshit Madan <akmadan1717@gmail.com>
2022-02-04 21:28:48 +0530

    private Keys

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -12,8 +12,7 @@ String adminAddress = '0x75936a0c47FeCEf2367BF8d16901C0B9A9c432b2';
 String infura_rinkbyURL =
     "https://rinkeby.infura.io/v3/fec8932d1dbe4c1ebe1a51559749ed8b";
 String contractAddress = "0x6B5902009c203943ab95Bb9c06116a8D9A341885";
-String adminPrivateKey =
-    "9e2c82ef68ad0baf4bc0fa84bbdc84e39e5f9a49d874cb635f895985ea45d2f8";
+String adminPrivateKey = adminPrivateKey; //secret
 
 List doctors = [
   [
